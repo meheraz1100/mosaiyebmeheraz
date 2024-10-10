0a49ad76bfe830ef2abde91ef6280c7c568301ac
meheraz1100 <meheraz1100@gmail.com>
2024-10-10 15:35:29 +0600

    email js for mail service

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -126,14 +126,15 @@ const projects = [
   {
     name: "EmployEase",
     description:
-      "A real-time collaborative whiteboard web application built with NextJs, Express, and Socket.io.",
+      "Employee Management Application with an interactive, Secure, Responsive, platform with Admin, HR, and Employee Dashboards.",
     features: [
-      "Users can create a room and invite others to join the room using a unique room code.",
-      "Users can draw on the whiteboard and the changes will be reflected in real-time to all the users in the room.",
+      "Admin can see the expenditure of whole month with Stats",
+      "And then control the unnecessary expenditure with 3X growth",
+      "A HR Officer can control the verified and unverified employees"
     ],
     tags: [
       {
-        name: "NextJs",
+        name: "react",
         color: "blue-text-gradient",
       },
       {
@@ -141,8 +142,8 @@ const projects = [
         color: "green-text-gradient",
       },
       {
-        name: "Socket.io",
-        color: "pink-text-gradient",
+        name: "mongodb",
+        color: "green-text-gradient",
       },
       {
         name: "Tailwind",
@@ -154,133 +155,51 @@ const projects = [
       },
     ],
     image: inkSync,
-    source_code_link: "https://github.com/Meheraz8443/InkSync",
-    live_link: "https://www.inksync.cloud/",
+    source_code_link: "https://github.com/meheraz1100/EmployEase-client",
+    live_link: "https://employ-ease.web.app/",
   },
   {
-    name: "Hayasaka",
+    name: "Job Nest",
     description:
-      "A dynamic full-stack music streaming web application with Next.js 13 and MongoDB.",
+      "A Job Posting Site that allows users to register or log in,including via Gmail. Users can browse and view detailed job listings, apply instantly, and track applications on an 'Applied Jobs' page.",
     features: [
-      "User authentication using NextAuth.js with JWT and OAuth providers.",
-      "Redux to Manage app state effectively.",
-      "Play, pause, skip, and control the playback of songs.",
-      "Users can Create, update and delete their own playlists.",
-      "Add songs to favorites in one click.",
-      "Auto adds similar songs to the queue.",
-      "Swipe and touch gestures for touch-responsive devices.",
-      "Display song details such as title, artist, album, and album artwork.",
-      "PWA (Progressive Web App) support. ",
+      "A recruiter can post job for hiring people",
+      "If the recruiter have some mistake then he/she can immediately update the job in 'Update' Page",
+      "Secure Authentication With Firebase Auth.",
     ],
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-      {
-        name: "nextjs",
-        color: "orange-text-gradient",
-      },
-      {
-        name: "nextauth",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "redux",
-        color: "green-text-gradient",
-      },
-    ],
-    image: carrent,
-    source_code_link: "https://github.com/Meheraz8443/hayasaka",
-    live_link: "https://hayasaka.live/",
-  },
-  {
-    name: "Study Notion",
-    description: "Developed an educational technology website with CMS.",
-    features: [
-      "Implemented Email verification and using OTP user authentication and login/signup functionality using JWT for secure access to the platform.",
-      "Students can browse and purchase courses. Instructors can create new courses, edit existing courses, and delete courses they have created.",
-      "Integrated MongoDB database to store user information, course data, and progress tracking.",
-      "Employed RESTful API principles for seamless communication between the frontend and backend systems.",
-      "Razorpay payment integration.",
-      "PWA (Progressive Web App) support.",
-    ],
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
       },
       {
         name: "ExpressJs",
-        color: "pink-text-gradient",
+        color: "green-text-gradient",
       },
       {
         name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tailwind",
         color: "orange-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "NodeJs",
         color: "blue-text-gradient",
       },
-      {
-        name: "nodejs",
-        color: "green-text-gradient",
-      },
     ],
-    image: jobit,
-    source_code_link: "https://github.com/Meheraz8443/Study-Notion-master",
-    live_link: "https://studynotion-master.vercel.app/",
+    image: carrent,
+    source_code_link: "https://github.com/meheraz1100/job-nest-client",
+    live_link: "https://job-n-est.web.app/",
   },
   {
-    name: "FlipKart Price Tracker",
-    description:
-      "Chrome extension to track FlipKart product prices and send an alert to users when prices go below the user-selected price.",
+    name: "BistroBoss",
+    description: "A Restaurant Management Website where users can order food, make payments, and view their payment history. Admins can modify food descriptions and availability, and add new admins.",
     features: [
-      "Users can add products to the watchlist by clicking on the extension icon on the product page.",
-      "Users can set a price alert by entering the desired price for the product.",
-    ],
-    tags: [
-      {
-        name: "JavaScript",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "Chrome Extension",
-        color: "green-text-gradient",
-      },
-      {
-        name: "ReactJs",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tracker,
-    source_code_link: "https://github.com/Meheraz8443/FlipKart-Price-Tracker",
-    live_link:
-      "https://chromewebstore.google.com/detail/fliptrack-flipkart-price/pekpfcghlbljghaojdlajgpceebobokd?hl=en",
-  },
-  {
-    name: "Social Sphere",
-    description:
-      "A full-stack social media application with user authentication, infinite scroll, and image and video uploads.",
-    features: [
-      "User authentication using JWT and OAuth providers.",
-      "Infinite scroll for posts.",
-      "Image and video uploads.",
-      "Like, comment, and share posts.",
-      "Follow and unfollow users.",
-      "Dark mode support.",
+      "Admin can track the revenue of whole month.",
+      "User can order foods and pay the price and can see the history.",
+      "Security implemented with JSON WEB TOKEN (verify in Both Side).",
     ],
     tags: [
       {
@@ -288,25 +207,25 @@ const projects = [
         color: "blue-text-gradient",
       },
       {
-        name: "nodejs",
+        name: "ExpressJs",
         color: "green-text-gradient",
       },
       {
-        name: "Supabase",
-        color: "pink-text-gradient",
+        name: "mongodb",
+        color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "Tailwind",
         color: "orange-text-gradient",
       },
       {
-        name: "redux",
+        name: "NodeJs",
         color: "blue-text-gradient",
       },
     ],
-    image: socialSphere,
-    source_code_link: "https://github.com/Meheraz8443/SocialSphere",
-    live_link: "https://socialspherex.vercel.app/",
+    image: jobit,
+    source_code_link: "https://github.com/meheraz1100/bistro-boss-client",
+    live_link: "https://bistro-bo-ss.web.app/",
   },
 ];
 
