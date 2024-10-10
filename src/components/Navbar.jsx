3947f75a93ec822317e858947688cc48ad9ded43
meheraz1100 <meheraz1100@gmail.com>
2024-10-10 15:49:34 +0600

    some bugs are fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <Image src={logo} alt='logo' className='w-10 h-10 object-contain' />
+          <Image src={''} alt='' className='w-10 h-10 object-contain' />
           <p className='text-white text-[29px] font-bold cursor-pointer flex relative right-2 '>
             Meheraz &nbsp;
             <span className='sm:block hidden'> </span>
